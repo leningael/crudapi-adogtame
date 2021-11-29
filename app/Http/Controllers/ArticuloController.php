c32cd408f36bc4d4334296e422bf6badb3bc183f
Lenin Rosas <leningaelrs@gmail.com>
2021-11-29 02:51:29 -0500

    Creacion del controlador y modelo de articulo

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Articulo;
+
+class ArticuloController extends Controller{
+
+}
---
+++ app/Models/Articulo.php
@@ -0,0 +1,12 @@
+<?php   
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model{
+    protected $table = "articulos";
+
+    // protected $fillable = [];
+
+    // public $timestamps = false;
+}
